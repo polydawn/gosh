52ed14e452873c1c876d72cae966244fb656285d
Eric Myhre <hash@exultant.us>
2015-03-29 01:40:38 -0500

    Re-add the Start, Run, Output, and CombinedOutput methods for trigger execution.  Restore all the exit-code checking behave for Run.  Add documentation.  Give it basic test coverage via some simple examples.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- shell.go
+++ shell.go
@@ -1,5 +1,7 @@
 package gosh
 
+import "bytes"
+
 func Gosh(args ...interface{}) Command {
 	var cmdt Opts
 	cmdt.Launcher = ExecLauncher
@@ -12,6 +14,16 @@ func Gosh(args ...interface{}) Command {
 	Calling a `Command` merges in the arguments and then
 	immediately launches a `Proc`.  The `Command()` call waits for the `Proc`
 	to complete, panics if it fails, and finally returns the `Proc`.
+
+	The parameters can take many forms:
+	  - `string` or `[]string` types will be merged into the command args list.
+	  - `Env` types will be joined with the command environment variables.
+	  - `ClearEnv` will discard *all* current environment variables.
+	  - `Opts` objects can do all of the above, and also
+	    set the working directory,
+		set the input and output streams,
+		configure the "acceptable" exit codes,
+		and even inject a custom Proc Launcher.
 
 	Use `Command.Start()` to just launch and immediately return the `Proc`
 	if you want to do your own job control.
@@ -26,6 +38,9 @@ type Command func(args ...interface{}) Proc
 	The returned command can launch `Proc`s repeatedly or futher baked, just
 	like the original.
 
+	All the same types that `Command()` can accept, `Bake()` can accept too.
+	They have the same effects.
+
 	The returned command is a "deep copy" for everything except the In/Out/Err
 	readers/writers -- it's completely separated; further changes do not have
 	the power to mutate the original.
@@ -35,6 +50,68 @@ type Command func(args ...interface{}) Proc
 */
 func (c Command) Bake(args ...interface{}) Command {
 	return enclose(bake(c.expose(), args...))
+}
+
+/*
+	Starts execution of the command, and waits until completion before returning.
+	If the command does not execute successfully, a panic of type FailureExitCode
+	will be emitted; use `Opts.OkExit` to configure what is considered success.
+
+	The is exactly the behavior of a no-arg invokation on an Command, i.e.
+		`Gosh("echo")()`
+	and
+		`Gosh("echo").Run()`
+	are interchangable and behave identically.
+
+	The behavior of an invoking Command with parameters
+	is the same as baking in the extra parameters and then calling Run:
+		`Gosh("echo")("my", "story")`
+	and
+		`Gosh("echo", "my", "story").Run()`
+	and
+		`Gosh("echo").Bake("my", "story").Run()`
+	are all interchangable and behave identically.
+
+	Use the `Start()` method instead if you need to run a task in the
+	background, or otherwise need greater control over execution.
+*/
+func (c Command) Run() Proc {
+	return c.expose().run()
+}
+
+/*
+	Starts execution of the command, and immediately returns a `Proc` that
+	can be used to track execution of the command, configure exit listeners,
+	etc.
+*/
+func (c Command) Start() Proc {
+	return c.expose().start()
+}
+
+/*
+	Starts execution of the command, waits until completion, and then returns the
+	accumulated output of the command as a string.  As with `Run()`, a panic will be
+	emitted if the command does not execute successfully.
+
+	This does not include output from stderr; use `CombinedOutput()` for that.
+
+	This is shorthand equivalent to `Bake(Opts{Out:val}).Run()`; that is, it will
+	overrule any previously configured output, and also it has no effect on where
+	stderr will go.
+*/
+func (c Command) Output() string {
+	var buf bytes.Buffer
+	c.Bake(Opts{Out: &buf}).Run()
+	return buf.String()
+}
+
+/*
+	Same as `Output()`, but acts on both stdout and stderr.
+*/
+func (c Command) CombinedOutput() string {
+	var buf bytes.Buffer
+	c.Bake(Opts{Out: &buf, Err: &buf}).Run()
+	return buf.String()
 }
 
 type Opts struct {
@@ -113,21 +190,36 @@ func (x Opts) Merge(y Opts) Opts {
 	return x
 }
 
+func (cmdt Opts) start() Proc {
+	return cmdt.Launcher(cmdt)
+}
+
+func (cmdt Opts) run() Proc {
+	p := cmdt.start()
+	p.Wait()
+	exitCode := p.GetExitCode()
+	for _, okcode := range cmdt.OkExit {
+		if exitCode == okcode {
+			return p
+		}
+	}
+	panic(FailureExitCode{cmdname: cmdt.Args[0], code: exitCode})
+}
+
 type magic struct{ cmdt Opts }
 
 func enclose(cmdt Opts) Command {
+	// This is the actual implementation of `Command`.
 	return func(args ...interface{}) Proc {
 		if len(args) == 0 {
-			p := cmdt.Launcher(cmdt)
-			p.Wait()
-			return p
+			return cmdt.run()
 		}
 		switch magic := args[0].(type) {
 		case *magic:
 			magic.cmdt = cmdt
 			return nil
 		default:
-			return cmdt.Launcher(bake(cmdt, args...))
+			return bake(cmdt, args...).start()
 		}
 	}
 }
--- shell_examples_test.go
+++ shell_examples_test.go
@@ -1,14 +1,30 @@
 package gosh_test
 
 import (
+	"fmt"
 	"os"
 
 	. "github.com/polydawn/gosh"
 )
 
 func ExampleNormalFlow() {
-	cmd := Gosh("echo", "hello world!", Opts{Out: os.Stdout})
-	cmd()
+	Gosh("echo", "hello world!", Opts{Out: os.Stdout}).Run()
+
+	// Output:
+	// hello world!
+}
+
+func ExampleStartCollectFlow() {
+	proc := Gosh("echo", "hello world!", Opts{Out: os.Stdout}).Start()
+	proc.Wait()
+
+	// Output:
+	// hello world!
+}
+
+func ExampleCollectOutput() {
+	str := Gosh("echo", "hello world!").Output()
+	fmt.Println(str)
 
 	// Output:
 	// hello world!
